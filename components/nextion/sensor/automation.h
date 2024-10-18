0f4952b79588e94c27288ba701cdc6d2c66de94f
Petr Kejval <petr.kejval6@gmail.com>
2024-10-18 12:37:22 +0200

    test

---
+++ components/nextion/sensor/automation.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "esphome/core/automation.h"
+#include "nextion.h"
+
+namespace esphome {
+namespace nextion {
+
+template<typename... Ts> class NextionSensorPublishAction : public Action<Ts...> {
+ public:
+  NextionSensorPublishAction(NextionSensor *sensor) : sensor_(sensor) {}
+  TEMPLATABLE_VALUE(float, state)
+  TEMPLATABLE_VALUE(bool, publish)
+  TEMPLATABLE_VALUE(bool, send_to_nextion)
+
+  void set_state(Ts... x) override { this->sensor_->set_state(this->state_.value(x...), this->publish_.optional_value(x...), this->send_to_nextion_.optional_value(x...)); }
+
+ protected:
+  NextionSensor *sensor_;
+};
+
+}
+}
